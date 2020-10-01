629822e5a2316ae30afc812aaf7d10579d11b57c
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-01 13:28:03 +0300

    репозиторий для домашки 3-го модуля

---
+++ .js/task-1.js
@@ -0,0 +1,28 @@
+let message = '';
+const user = {
+    age: 20,
+    hobby: 'html',
+    name: 'Mango',
+    premium: true,
+};
+
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+user['full time'] = true;
+
+const keys = Object.keys(user);
+// console.log(Object.keys(user));
+const values = Object.values(user);
+// console.log(Object.values(user));
+
+for (let key of keys) {
+
+    // console.log('Key: ', user[key]);
+
+    let value = user[key];
+    message = `${key}: ${value}\n`
+    console.log(message);
+}
+
